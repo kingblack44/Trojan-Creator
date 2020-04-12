85b49b1e12d928ed653e77bcb96a3ac832c15f96
kingblack44 <53968012+kingblack44@users.noreply.github.com>
2020-04-12 11:53:56 +0700

    Add files via upload

---
+++ Trojan-Creator.sh
@@ -0,0 +1,228 @@
+# MAU RECODE YA?
+# SILAHKAN KALO LU BISA RECODE :V
+# AUTO BERANTAKAN WKWKWKKW TOLOL
+# ========== Subscribe Ariel 69 Channel =======
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+m="\033[31;1m"
+b="\033[34;1m"
+p="\033[37;1m"
+xdg-open https://www.youtube.com/channel/Ariel69Channel
+echo
+echo
+echo
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo " [*] (Ctrl + C ) Detected, Trying To Exit ... "
+echo " [*] Thanks"
+sleep 1
+echo " [*] Semoga Bermanfaat "
+echo " [*] By Ariel Sandy Permana :)..."
+sleep 1
+exit
+}
+lagi=1
+while [ $lagi -lt 14 ];
+do
+clear
+echo "\033[1;31m  ╭═══════════════════════════════════════════════════╮"
+echo "\033[1;35m•\033[1;31m─║>\033[1;32m Author : \033[1;33mAriel Sandy Permana [ TAMPAN ]\033[1;31m          ║─\033[1;35m•"
+echo "\033[1;31m  ║>\033[1;32m YouTube : [1;33mAriel 69 Channel                       \033[1;31m║"
+echo "\033[1;31m  ║>\033[;36m Tools Name : \033[1;32mTROJAN \033[1;37mCREATOR                      \033[1;31m║"
+echo "\033[1;31m  ║>\033[;36m Tools Version : \033[1;32m0.1 \033[1;90mNew                          \033[1;31m║"
+echo "\033[1;35m•\033[1;31m─║>\033[1;32m Facebook : \033[1;33mCorona                                \033[1;31m║─\033[1;35m•"
+echo "\033[1;31m  ╰═══════════════════════════════════════════════════╯"
+echo "\033[1;31m  ╭══════════════════════════════╮"
+echo "\033[1;35m•\033[1;31m─║ (1) \033[1;33mCreate Script Trojans    \033[1;31m║─\033[1;35m•"
+echo "\033[1;35m•\033[1;31m─║ (2) \033[1;33mMelihat Hasil Trojans    \033[1;31m║─\033[1;35m•"
+echo "\033[1;35m•\033[1;31m─║ (3) \033[1;33mJalankan Trojans         \033[1;31m║─\033[1;35m•"
+echo "\033[1;35m•\033[1;31m─║ (0) \033[1;90mKeluar                   \033[1;31m║─\033[1;35m•"
+echo "\033[1;31m  ╰╬═════════════════════════════╯"
+read -p   "   ╚═══•> " pil;
+case $pil in
+1) echo ""
+echo ""
+cp x.py /sdcard
+cp Ariel.flf /sdcard
+clear
+printf " ${m}____________
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║${h}
+${b}╔════════════╗${h}
+${b}╚════════════╝${h}
+${b} ║${h}██████████${b}╚╗${h}
+${b} ║${h}██${m}╔══╗${h}█${m}╔═╗${h}█${b}║${h}
+${b} ║${h}██${m}║${k}╬${m}╔╝${h}█${m}╚╗║${h}█${b}║${h}
+${b} ║${h}██${m}╚═╝${h}█${m}║${h}█${m}╚╝${h}█${b}║${p}
+${b} ╚╗${h}█████████${b}═╝
+${b}  ╚╗║${m}╠╩╩╩╩╩╝
+${b}   ║║${h}┈┈┈█${b}▐█████${m}▒${p}.｡oO
+${b}   ║${h}██${m}╠╦╦╦╗
+${b}   ╚╗${h}██████${p} Author${m}:${b}Ariel Sandy Permana
+${b}    ╚════╝${p}Team${m}:${b}Sendiri Tetap Santuy
+${p}<${k}════════════════${p}X${k}═════════════════${p}>
+"
+printf "
+${p}[${h}•${p}]${h}MASUKAN NAMA Script${m}: ${p}"
+read sc;
+cd /sdcard
+rm -rf $sc
+touch $sc
+echo "import sys, os" >> $sc
+echo "from time import sleep" >> $sc
+echo "os.system('clear')" >> $sc
+echo 'os.system("figlet -f Ariel TROJAN | lolcat")' >> $sc
+echo 'target = raw_input("\x1b[32;1mMasukan Hp Korban \x1b[31;1m :\x1b[33;1m ") ' >> $sc
+echo "sleep(1)" >> $sc
+echo "os.system('clear')" >> $sc
+echo 'os.system("figlet -f Ariel TROJAN | lolcat")' >> $sc
+echo "print '\x1b[32;1mTarget Found ! '" >> $sc
+echo "print '\x1b[31;1mStart Attack \x1b[33;1m' + target + '\x1b[31;1m ?'" >> $sc
+echo "lanj = raw_input('\x1b[32;1mLu Yakin Ini Nomer Nya ? (Y/N)\x1b[33;1m = ') " >> $sc
+echo "if lanj == 'y': " >> $sc
+echo "    while True: " >> $sc
+echo "        print '\x1b[32;1mSubscribe Ariel 69 Channel Berhasil Mengirim Ke\x1b[31;1m :\x1b[33;1m ' + target" >> $sc
+echo
+sleep 1
+clear
+python2 x.py
+sleep 2
+echo
+echo $p2 "ingin Membuat Script Trojans ? [Y] / Coba yang lain [T] ($p2"Y/T"$p2)"$p2"
+"
+read -p "Ariel Sandy Permana~# " pil3;
+if [ $pil3 = y ] || [ $pil3 = Y ];
+then
+clear
+echo "[*] Script Tersimpan Di /sdcard/$sc" | lolcat
+sleep 1
+echo "[*] Subscribe Ariel 69 Channel" | lolcat
+sleep 3
+echo "[*] Please Di Subscribe Broooo" | lolcat
+sleep 3
+echo "[*] P" | lolcat
+clear
+sleep 3
+else
+echo "Terima Kasih" | lolcat
+clear
+rm -rf $sc
+cd /sdcard
+rm -rf $sc
+sleep 2
+fi
+;;
+2) echo ""
+echo ""
+cd /sdcard
+clear
+printf " ${m}____________
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║${h}
+${b}╔════════════╗${h}
+${b}╚════════════╝${h}
+${b} ║${h}██████████${b}╚╗${h}
+${b} ║${h}██${m}╔══╗${h}█${m}╔═╗${h}█${b}║${h}
+${b} ║${h}██${m}║${k}╬${m}╔╝${h}█${m}╚╗║${h}█${b}║${h}
+${b} ║${h}██${m}╚═╝${h}█${m}║${h}█${m}╚╝${h}█${b}║${p}
+${b} ╚╗${h}█████████${b}═╝
+${b}  ╚╗║${m}╠╩╩╩╩╩╝
+${b}   ║║${h}┈┈┈█${b}▐█████${m}▒${p}.｡oO
+${b}   ║${h}██${m}╠╦╦╦╗
+${b}   ╚╗${h}██████${p} Author${m}:${b}Ariel Sandy Permana
+${b}    ╚════╝${p}Team${m}:${b}Sendiri Tetap Santuy
+${p}<${k}════════════════${p}X${k}═════════════════${p}>
+"
+echo "Silahkan Ketikan Sc Yang Tadi Anda Buat" | lolcat
+printf "
+${p}[${h}•${p}]${h}${m}>> ${p}"
+read sc;
+echo
+sleep 1
+clear
+echo "Silahkan Kalian Ketikan Y Untuk Melihat Hasil" | lolcat
+sleep 2
+echo
+echo $p2 "ingin Melihat Script Trojans Yang Anda Buat Tadi ? [Y] /[T] ($p2"Y/T"$p2)"$p2"
+"
+read -p "Ariel Sandy Permana~# " pil3;
+if [ $pil3 = y ] || [ $pil3 = Y ];
+then
+echo
+clear
+cat $sc
+sleep 3
+else
+echo "Terima Kasih" | lolcat
+sleep 1
+fi
+exit
+;;
+3) echo ""
+echo ""
+cd /sdcard
+clear
+printf " ${m}____________
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║
+${m} ║${m}▒▒▒▒▒▒▒▒▒▒${m}║${h}
+${b}╔════════════╗${h}
+${b}╚════════════╝${h}
+${b} ║${h}██████████${b}╚╗${h}
+${b} ║${h}██${m}╔══╗${h}█${m}╔═╗${h}█${b}║${h}
+${b} ║${h}██${m}║${k}╬${m}╔╝${h}█${m}╚╗║${h}█${b}║${h}
+${b} ║${h}██${m}╚═╝${h}█${m}║${h}█${m}╚╝${h}█${b}║${p}
+${b} ╚╗${h}█████████${b}═╝
+${b}  ╚╗║${m}╠╩╩╩╩╩╝
+${b}   ║║${h}┈┈┈█${b}▐█████${m}▒${p}.｡oO
+${b}   ║${h}██${m}╠╦╦╦╗
+${b}   ╚╗${h}██████${p} Author${m}:${b}Ariel Sandy Permana
+${b}    ╚════╝${p}Team${m}:${b}Sendiri Tetap Santuy
+${p}<${k}════════════════${p}X${k}═════════════════${p}>
+"
+echo "Silahkan Ketikan Sc Yang Tadi Anda Buat" | lolcat
+printf "
+${p}[${h}•${p}]${h}${m}>> ${p}"
+read sc;
+echo
+sleep 1
+clear
+echo "Silahkan Kalian Ketikan Y Untuk Menjalankan Script" | lolcat
+sleep 2
+echo
+echo $p2 "ingin Menjalankan Script Trojan ? [Y] /[T] ($p2"Y/T"$p2)"$p2"
+"
+read -p "Ariel Sandy Permana~# " pil3;
+if [ $pil3 = y ] || [ $pil3 = Y ];
+then
+echo
+clear
+python2 $sc
+sleep 3
+else
+echo "Terima Kasih" | lolcat
+sleep 1
+fi
+exit
+;;
+0) echo " [*] Created By : Ariel Sandy Permana [ Author Autis :v ]"
+sleep 1
+echo " [*] Terima Kasih BUAT KALIAN SEMUA BANGSAT"
+sleep 1
+exit
+;;
+*) echo " [!] Pilih YANG BENER TOLOL"
+clear
+sleep 2
+esac
+done
+done
